fd4151787b9eec441d38bcc24ac48df67ceb6591
Hakim Mazouz <Hakim@Hakims-MacBook-Air.local>
2016-09-25 22:04:43 +0200

    remove redundant if statement, add comments to menu.php

--- includes/menu.php
+++ includes/menu.php
@@ -2,29 +2,32 @@
 
 // Get the current page by checking what the property "page" of what the get request is
 $currentPage = isset($_GET['page']) ? $_GET['page'] : 'home';
-$pages = scandir('pages/');
-$pages = array_splice($pages, 2);
 
-if (isset($_GET['page'])) {
-	$currentPage = $_GET['page'];
-} else {
-	$currentPage = 'home';
-}
+// scan the pages directory for pages
+$pages = scandir('pages/');
+
+// we remove the first to items in the pages array that consist of references to the current directory and the parent directory
+$pages = array_splice($pages, 2);
 
 ?>
 
 <header>
 	<div class="container">
+		<!-- we set the hyperlink reference to be equal to the hostname and subdirectory php_menu -->
 		<a class="logo" href="<?= 'http://' . $_SERVER['HTTP_HOST'] . '/php_menu' ?>">Panels<sup>&copy;</sup></a>
 		<ul>
 			<?php 
-
+				// we initiate a foreach loop where we loop over our array of pages
 				foreach ($pages as $page) {
+
+					// we start by stripping out the file extension on the item
 					$page = basename($page,".php");
 
 					?>
+						<!-- then, we set the reference to be a get paremeter followed by the title of the page. the class name is an if statement which sets the active class if the condition of the current page being equal to the page we are looping over evaluates to true. -->
 
-						<li><a href="<?= '?page=' . $page ?>" class="<?= $currentPage == $page ? 'active' : '' ?>"><?= ucfirst($page) ?></a></li>
+						<!-- afterwards, we capitalize the first letter of the page name and put that as the menu item name -->
+						<li><a href="<?= '?page=' . $page ?>" <?= $currentPage == $page ? 'class="active"' : '' ?>><?= ucfirst($page) ?></a></li>
 
 					<?php
 				}
